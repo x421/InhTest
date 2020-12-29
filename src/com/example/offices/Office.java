4172e24e5340f296c7a1b8fd16e9b28933533f9b
x421 <mr.dubstebmix@gmail.com>
2020-12-29 16:31:41 +0300

    final

--- src/com/example/offices/Office.java
+++ src/com/example/offices/Office.java
@@ -5,21 +5,29 @@
  * @version 1.0
  */
 
-package classes;
+package com.example.offices;
+
+import com.example.cabinets.Cabinet;
 
 import java.util.ArrayList;
 
 public class Office {
+    /** Список всех кабинетов офиса */
     private ArrayList<Cabinet> officeCabinets;
 
     public Office() {
         officeCabinets = new ArrayList<Cabinet>();
     }
 
+    /**
+     * Добавление кабинета в офис
+     * @param c Кабинет
+     */
     public void addCabinet(Cabinet c) {
         officeCabinets.add(c);
     }
 
+    /** Печать информации об оффисе на экран */
     public void printOfficeInfo() {
         for(int end = officeCabinets.size(), i = 0; i < end; i++) {
             officeCabinets.get(i).printCabinetInfo();
